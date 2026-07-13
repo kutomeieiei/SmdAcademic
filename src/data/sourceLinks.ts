e3b68b0fd95690816d533aff0feed756dbc993e0
kutomeieiei <kutomei38@gmail.com>
2026-07-13 11:31:19 +0700

    Update sourceLinks.ts

--- src/data/sourceLinks.ts
+++ src/data/sourceLinks.ts
@@ -14,5 +14,61 @@ export const sourceLinks: ExternalLinkItem[] = [
     description: 'Grammar 16000 ข้อพร้อมเฉลยโดย Atalay Oguz',
     url: ' https://staff.tiiame.uz/storage/users/247/books/GnGozbEwKKuzJeFZwlP44jd4P7XPAoUUz9lMqJEX.pdf',
     tags: ['อังกฤษ', 'เเบบฝึกหัด']
+  },
+  {
+    id: 'src3',
+    title: 'Bio.Krit',
+    description: 'GiveAway สรุปชีวะบทต่างๆสำหรับเข้าค่ายหนึ่งจนไปถึง TBO',
+    url: 'https://www.instagram.com/bio.krit/',
+    tags: ['ชีวะ', 'สรุปเนื้อหา']
+  }
+  {
+    id: 'src4',
+    title: 'finalcall.tutor',
+    description: 'ไฟล์สรุป GiveAway เก็งข้อสอบ เทคนิคการเก็บเนื้อหาชีววิทยาจากติวเตอร์เเพทย์ศิริราช',
+    url: 'https://www.instagram.com/finalcall.tutor/',
+    tags: ['ชีวะ', 'สรุปเนื้อหา']
+  },
+  {
+    id: 'src5',
+    title: 'SmartMathPro',
+    description: 'ติวเนื้อหาคณิตศาสตร์ฟรีใน Youtube โดยพี่ปั้น',
+    url: 'https://www.youtube.com/@SmartMathPro',
+    tags: ['คณิต', 'สรุปเนื้อหา']
+  },
+  {
+    id: 'src6',
+    title: 'I know PHYSICs a little',
+    description: 'คริปเนื้อหาฟิสิกส์เเบบเร่งรัด เข้าใจง่าย',
+    url: 'https://www.youtube.com/@I.know.physics.a.little',
+    tags: ['ฟิสิกส์', 'สรุปเนื้อหา']
+  },
+  {
+    id: 'src7',
+    title: 'lazybio',
+    description: 'ชีววิทยาสำหรับคนขี้เกียจอ่านหนังสือ',
+    url: 'https://www.youtube.com/@lazybio',
+    tags: ['ชีวะ', 'สรุปเนื้อหา']
+  },
+  {
+    id: 'src8',
+    title: 'พี่ดราฟ Math_draft',
+    description: 'รวบรวมความรู้เเละเทคนิคในวิชาคณิตศาสตร์เเละฟิสิกส์',
+    url: 'https://www.instagram.com/math_draft/',
+    tags: ['คณิต', 'ฟิสิกส์', 'สรุปเนื้อหา']
+  },
+  {
+    id: 'src9',
+    title: 'Cardsiaceae',
+    description: 'GiveAway เนื้อหาชีวะบทต่างๆ',
+    url: 'https://www.instagram.com/cardsiaceae/',
+    tags: ['ชีวะ', 'สรุปเนื้อหา']
+  },
+  {
+    id: 'src10',
+    title: 'ฟิสิกส์โกเอก',
+    description: 'ช่องทางการเรียนรู้เนื้อหาฟิสิกส์ฟรี (คริป)',
+    url: 'https://www.youtube.com/@physicskoake',
+    tags: ['ฟิสิกส์', 'สรุปเนื้อหา']
   }
 ];
